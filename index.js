ee8d17fd05bc9550e8e1ef5ae105e6562307edfb
phong <buitrongphonghp@gmail.com>
2023-08-23 01:23:05 +0700

    update code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + 'index.html');
 });
 console.log(__dirname + '/public')
 
